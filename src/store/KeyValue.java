fa6b4d1c0ad27352e301d208fbbe147a2f7f211e
Your Name <ermolaeva@agent.ru>
2017-05-09 20:09:36 +1000

    single responsibility for KeyValue storage

---
+++ src/store/KeyValue.java
@@ -0,0 +1,24 @@
+package store;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyValue {
+    public static final int matrix = 5;
+    public static final Map<String, int[]>   vecAliaces;
+    public static final Map<String, Integer> dirAliaces;
+
+    static {
+        vecAliaces = new HashMap<>(5);
+        vecAliaces.put("NORTH", new int[]{0, 1});
+        vecAliaces.put("EAST", new int[]{-1, 0});
+        vecAliaces.put("SOUTH", new int[]{1, 0});
+        vecAliaces.put("WEST", new int[]{0, -1});
+
+        dirAliaces = new HashMap<>(5);
+        dirAliaces.put("LEFT", -90);
+        dirAliaces.put("RIGHT", 90);
+    }
+
+}
