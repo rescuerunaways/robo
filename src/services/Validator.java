0a18bd35a5cee3a969791441daffad6f48cea089
Your Name <ermolaeva@agent.ru>
2017-05-10 23:03:15 +1000

    added validator

---
+++ src/services/Validator.java
@@ -0,0 +1,13 @@
+package services;
+
+
+import model.Command;
+
+public class Validator {
+    public static boolean validate(Command c) {
+        //if move - check if we are going to fall out
+        switch (c.getName()) {
+        }
+        return true;
+    }
+}
